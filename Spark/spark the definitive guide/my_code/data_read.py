c82d21b5a78cf63d93dcb84812d44489c79f18e6
Piyush Ramkar <ramkarpiyush@gmail.com>
2025-06-20 01:25:27 +0530

    .

---
+++ Spark/spark the definitive guide/my_code/data_read.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[*]").appName("5 Basic Structured Operations").getOrCreate()
+
+from pyspark.sql.types import StructType, StructField, StringType, LongType
+my_manual_schema = StructType([
+    StructField("DEST_COUNTRY_NAME", StringType(), True),
+    StructField("ORIGIN_COUNTRY_NAME", StringType(), True),
+    StructField("count", LongType(), False)
+])
+
+df = spark.read.format("json").schema(my_manual_schema).load("D:\\GitLocal\\Spark-The-Definitive-Guide\\data\\flight-data\\json\\2015-summary.json")
+df.printSchema()
+df.show(1)
