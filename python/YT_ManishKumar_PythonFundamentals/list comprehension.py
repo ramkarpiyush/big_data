f790c534ad0c1ef713a4d4acfebf1cf6128d7038
Piyush Ramkar <ramkarpiyush@gmail.com>
2025-06-23 01:15:04 +0530

    Python Dictionary added

--- python/YT_ManishKumar_PythonFundamentals/list comprehension.py
+++ python/YT_ManishKumar_PythonFundamentals/list comprehension.py
@@ -13,3 +13,25 @@ for i in numbers:
 
 print(emp)
 print(emp2)   
+
+# Method 2:
+new_list = []
+new_list = [i for i in range(1, 11) if i%2==0]
+print(new_list)
+
+
+# Method 1:
+new_list2 = []
+for i in range(1, 11):
+    if i%2 == 0:
+        new_list2.append("Even")
+    else:
+        new_list2.append("Odd")
+    
+print(new_list2)
+
+# Method 2:
+new_list2 = []
+
+new_list2= ["Even" if i%2==0 else "Odd" for i in range(1, 11)]
+print(new_list2)
